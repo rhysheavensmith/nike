41c4c0661f7288ab9444ea33142970cc701a215e
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2024-01-15 20:22:09 +1300

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+
+export {
+	Hero,
+	Footer,
+	SuperQuality,
+	PopularProducts,
+	Services,
+	SpecialOffers,
+	CustomerReviews,
+	Subscribe,
+};
