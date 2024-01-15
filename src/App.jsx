59b83169e56e87fd13a18e278c4b29cc05cf9b71
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2024-01-15 22:06:48 +1300

    finished the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import {
 	Subscribe,
 } from './sections';
 
+import Nav from './components/Nav';
+
 const App = () => {
 	return (
 		<main className='relative'>
-			{/* {<Nav/>} */}
+			<Nav />
 			<section className='xl:padding-1 wide:padding-r padding-b'>
 				<Hero />
 			</section>
